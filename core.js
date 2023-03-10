ae5f502534f89d903c6d23953c180a005a596251
RomarioBarahona <84294268+RomarioBarahona@users.noreply.github.com>
2023-03-10 04:51:49 -0500

    update + dark/light mode
    .ico logo added and have dark/light mode working based on your system settings

--- core.js
+++ core.js
@@ -5,3 +5,18 @@ const nav = document.querySelector("nav");
 navIcon.onclick = function () {
     nav.classList.toggle('show');
 }
+
+//Detects Dark or Light mode
+let dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+console.log(matched);
+
+let body = document.querySelector("body");
+let content = document.getElementById("content");
+
+if (dark) {
+	body.style.backgroundColor = "#1a1a1a";
+	content.setAttribute("class", "dark");
+} else {
+	body.style.backgroundColor = "#f5f5f5";
+	content.setAttribute("class", "light");
+}
